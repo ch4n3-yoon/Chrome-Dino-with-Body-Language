d8813d2b88b83cc79688ec89e02285a8afbf4b31
ch4n3-yoon <ch4n3.yoon@gmail.com>
2020-06-07 17:14:01 +0900

    [Edit] - Add CORS setting into Flask server

--- Web/server.py
+++ Web/server.py
@@ -6,15 +6,18 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 import keyboard
 from flask import Flask, render_template
+from flask_cors import CORS
 import threading
+import time
 
 
 app = Flask(__name__)
+CORS(app, resources={r'*': {'origins': '*'}})
 
-
-@app.route('/jump')
+@app.route('/jump')     
 def jump():
     keyboard.send_spacebar()
+    time.sleep(1)
     return ''
 
 
@@ -23,6 +26,14 @@ def cam():
     return render_template('tf_cam.html')
 
 
+@app.route('/game')
+def game():
+    return render_template('chrome-dino.html')
+
+
 def init_server():
-    # thread.start_new_thread(flaskThread, ())
     threading.Thread(target=app.run).start()
+
+
+if __name__ == '__main__':
+    app.run()
